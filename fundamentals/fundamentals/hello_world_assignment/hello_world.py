884f262d8371554b20bd99ab49066452e759c4e6
nicholasgoodwin3 <nicholasgoodwin3@gmail.com>
2022-02-13 06:22:38 -0500

    edit

--- fundamentals/fundamentals/hello_world_assignment/hello_world.py
+++ fundamentals/fundamentals/hello_world_assignment/hello_world.py
@@ -12,6 +12,6 @@ print("hello", str(42), "!")
 food="grapes"
 food_2="apples"
 
-print("I love to eat {} and {}.".format(x,y))
-print(f"I love to eat {x} and {y}")
+print("I love to eat {} and {}.".format(food,food_2))
+print(f"I love to eat {food} and {food_2}")
 
